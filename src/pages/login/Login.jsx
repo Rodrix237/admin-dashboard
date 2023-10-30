39458085085ea54a04e3c473f0dc8449ae216740
Rod <rodrigue@upowa.org>
2023-10-30 18:56:38 +0100

    authentification

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,115 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
+import logo from "../../assets/logo.png";
+import { useNavigate } from "react-router-dom";
+import { message, Form, Input, Button } from "antd";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    sessionStorage.clear();
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password);
+
+    if (password === "admin") {
+      sessionStorage.setItem("email", email);
+      message.success("Connexion réussie");
+      navigate("/");
+    } else {
+      message.warning("Le mot de passe  est incorrect.");
+    }
+  };
+
   return (
     <div className="bgLogin">
-      <h1>Login</h1>
+      <div className="container">
+        <div className="container-form">
+          <div className="imageform">
+            {" "}
+            <img src={logo} alt="logo" />
+          </div>
+
+          <div className="form-login">
+            <h1>
+              Welcome back to <br />
+              <font color="blue">ROD</font>
+            </h1>
+            <small>Sign in to your account below</small>
+            <Form onSubmit={handleSubmit}>
+              <div className="field-input">
+                <Form.Item
+                  name={"email"}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please, enter your email",
+                    },
+                    {
+                      type: "email",
+                      message: "Email is not valid",
+                      warningOnly: true,
+                    },
+                  ]}
+                >
+                  <Input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    id="email"
+                    style={{ width: "400px" }}
+                    prefix={<MailOutlined />}
+                  />
+                </Form.Item>
+              </div>
+              <div className="field-input">
+                <Form.Item
+                  name={"password"}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please, enter your password",
+                    },
+                    {
+                      type: "password",
+                      message: "password is not valid",
+                      warningOnly: true,
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    placeholder="Password"
+                    className="input"
+                    id="password"
+                    style={{ width: "400px" }}
+                    prefix={<LockOutlined />}
+                  />
+                </Form.Item>
+              </div>
+              <Button
+                htmlType="submit"
+                type="primary"
+                style={{ width: "150px" }}
+                onClick={handleSubmit}
+              >
+                Log In
+              </Button>
+            </Form>
+          </div>
+        </div>
+        <div className="container-image">
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
