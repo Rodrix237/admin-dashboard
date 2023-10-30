7bd352e1887e1b4e808ffdd116db52397a891623
Rod <rodrigue@upowa.org>
2023-10-30 19:05:32 +0100

    Restrict access to pages by authentication

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let email = sessionStorage.getItem("email");
+    if (email === "" || email === null) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
