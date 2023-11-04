d96810cd07bd4475e242f541494403873b6a73ff
Rod <rodrigue@upowa.org>
2023-11-04 11:17:37 +0100

    Fetch Users

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Table } from "antd";
 
 const User = () => {
+  const url = "http://localhost:3000/user/all";
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let email = sessionStorage.getItem("email");
+    if (email === "" || email === null) {
+      navigate("/login");
+    }
+    getUsers();
+  }, []);
+
+  const columns = [
+    {
+      title: "Prénom",
+      dataIndex: "firstname",
+      key: "firstname",
+    },
+    {
+      title: "Nom",
+      dataIndex: "lastname",
+      key: "lastname",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Profile",
+      dataIndex: "profil",
+      key: "profil",
+    },
+  ];
+
+  const getUsers = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(resp.data);
+        setUsers(resp.data);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
-      <h1>User</h1>
+      <h2 style={{ marginLeft: "20px" }}>Utilisateurs</h2>
+      <div style={{ marginLeft: "20px", width: "80vw", marginTop: "20px" }}>
+        <Table loading={loading} dataSource={users} columns={columns} />
+      </div>
     </div>
   );
 };
